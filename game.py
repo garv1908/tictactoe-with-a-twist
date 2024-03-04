59a07b509b8fcc6165b74a90d753e702f6978551
Garv <garv1908@gmail.com>
2024-03-04 17:39:56 -0500

    completed the basic game!

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ board = ["-", "-", "-",
 currentPlayer = "x"
 gameRunning = True
 winner = None
+player = "x"
 
 def playerInput(board):
     try:
@@ -32,9 +33,10 @@ def computerPlay(board):
     pos = random.randint(0, 8)
     if board[pos] == "-":
         board[pos] = currentPlayer
+        print("Computer thinking: ...aha!")
+        printBoard(board)
     else:
         computerPlay(board)
-    printBoard(board)
 
 def switchPlayer():
     global currentPlayer
@@ -43,13 +45,49 @@ def switchPlayer():
     else:
         currentPlayer = "x"
 
+class checkWin:
+    def checkTie(board):
+        global gameRunning
+        if "-" not in board:
+            print("It's a draw!")
+            gameRunning = False
+
+    def checkRow(board):
+        # if board[0] == currentPlayer and board[1] == currentPlayer and board[2] == currentPlayer or board[3] == currentPlayer and board[4] == currentPlayer and board[5] == currentPlayer or board[6] == currentPlayer and board[7] == currentPlayer and board[8] == currentPlayer:
+        for i in range(0, 6, 3):
+            if all(board[i + j] == currentPlayer for j in range(3)): 
+                return True
+
+    def checkColumn(board):
+        for i in range(3):
+            if all(board[i + j*3] == currentPlayer for j in range(3)): 
+                return True
+    
+    def checkDiagonal(board):
+        if (board[0] == board[4] == board[8] == currentPlayer) or \
+           (board[2] == board[4] == board[6] == currentPlayer):
+            return True
+    
+    def checkWin(board):
+        global player
+        global gameRunning
+        if checkWin.checkRow(board) or checkWin.checkColumn(board) or checkWin.checkDiagonal(board):
+            print(f"{currentPlayer.capitalize()} wins!")
+            gameRunning = False
+        elif checkWin.checkTie(board):
+            if currentPlayer != player:
+                print("Your turn now, dear human.")
+
 
 printBoard(board)
+
 while gameRunning:
     playerInput(board)
-    switchPlayer()
-    print("Computer thinking: ...aha!")
-    computerPlay(board)
-    switchPlayer()
-    checkWin(board)
-    print("Your turn now, dear human.")
+    checkWin.checkWin(board)
+
+    if gameRunning:
+        switchPlayer()
+        computerPlay(board)
+        checkWin.checkWin(board)
+        switchPlayer()
+        
